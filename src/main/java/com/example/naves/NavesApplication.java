bf6c816a7e76d4cec16030f7fd921cbc28ebebe2
Stepanyk <andrejstepanuk16@gmail.com>
2021-02-23 18:09:33 +0300

    site

---
+++ src/main/java/com/example/naves/NavesApplication.java
@@ -0,0 +1,12 @@
+package com.example.naves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/naves/NavesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.naves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
