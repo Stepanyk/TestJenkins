610ae1c18102574f40f1b631126d81534584c5e2
Stepanyk <andrejstepanuk16@gmail.com>
2021-02-24 00:17:38 +0300

    a

--- src/test/java/com/example/naves/NavesApplicationTests.java
+++ src/test/java/com/example/naves/NavesApplicationTests.java
@@ -2,13 +2,18 @@ package com.example.naves;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.Assert;
 
 @SpringBootTest
 class NavesApplicationTests {
 
 	@Test
 	void contextLoads() {
-
+		boolean a;
+        if(2<3) {
+        	a=false;
+		}
+		Assert.assertTrue(a);
 	}
 
 }
